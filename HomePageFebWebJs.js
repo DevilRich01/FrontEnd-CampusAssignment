df0bb2b395f4a7c5096a5ae74cbc533236db65bc
Febrina Ade <102131340+DevilRich01@users.noreply.github.com>
2022-11-21 22:20:32 +0700

    Add files via upload
    there is media in it, because the capacity is too large, those who open this can ask me via email febrinaade499@gmail.com

---
+++ HomePageFebWebJs.js
@@ -0,0 +1,16 @@
+//Body Picture Art
+let panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        deactivate();
+        panel.classList.add('active')
+    })
+});
+
+function deactivate(){
+    panels.forEach(panel=>{
+        panel.classList.remove('active')
+    })
+}
+
